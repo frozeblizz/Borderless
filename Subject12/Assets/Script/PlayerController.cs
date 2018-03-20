0c068304584f6eabcae84bb4696471f65ad8f35f
Frozeblizz <netitorn_y@sci.tu.ac.th>
2018-03-20 21:11:08 +0700

    possess mechanic fix

--- Subject12/Assets/Script/PlayerController.cs
+++ Subject12/Assets/Script/PlayerController.cs
@@ -6,61 +6,48 @@ public class PlayerController : MonoBehaviour
 {
     public int moveSpeed = 30;
     Rigidbody2D rigid;
-    SpriteRenderer sprite;
-    PlayerController control;
-    public Controller AIcontrol;
-    bool possessed = false;
+    public SpriteRenderer sprite;
+    public PlayerController control;
 
-     GameObject cache;
-    
+    public GameObject cache;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         control = GetComponent<PlayerController>();
         rigid = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            
+
             rigid.AddForce(new Vector2(-moveSpeed, 0));
 
         }
         if (Input.GetKey(KeyCode.D))
         {
-            
+
             rigid.AddForce(new Vector2(moveSpeed, 0));
-           
+
         }
         if (Input.GetKey(KeyCode.W))
         {
-            
+
             rigid.AddForce(new Vector2(0, moveSpeed));
-           
+
         }
         if (Input.GetKey(KeyCode.S))
         {
-            
+
             rigid.AddForce(new Vector2(0, -moveSpeed));
-            
+
         }
 
-        if (cache != null)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                cache.SetActive(true);
-                control.enabled = false;
-                cache.transform.position = transform.position;
-                cache = null;
-               
-            }
-        }
-      
+
     }
 
     private void OnTriggerStay2D(Collider2D hitWith)
@@ -72,13 +59,13 @@ public class PlayerController : MonoBehaviour
                 print(Vector3.Distance(transform.position, hitWith.transform.position));
                 if (Vector3.Distance(transform.position, hitWith.transform.position) <= 0.5f)
                 {
-                    AIcontrol.enabled = true;
+                    hitWith.gameObject.GetComponent<Controller>().enabled = true;
+                    
                     sprite.enabled = false;
                     control.enabled = false;
-                    //Control.enabled = true;
-                    possessed = true;
-                    StartCoroutine(GTFO(hitWith.gameObject));
-                   
+
+                    StartCoroutine(GTFO(gameObject));
+
                 }
             }
         }
