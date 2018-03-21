ba8a015b905ec6881e7eadf67f05a422bce86a3a
Phee Mongkon <phee_m@sci.tu.ac.th>
2018-03-21 13:51:14 +0700

    Enemy spawn point

---
+++ Subject12/Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Walkway").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+}
