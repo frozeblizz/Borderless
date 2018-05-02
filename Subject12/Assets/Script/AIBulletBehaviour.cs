2826731808115604ae5a44c780c5818e0268345c
Frozeblizz <netitorn_y@sci.tu.ac.th>
2018-05-02 15:19:18 +0700

    Bullet revert

---
+++ Subject12/Assets/Script/AIBulletBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBulletBehaviour : MonoBehaviour
+{
+    public GameObject[] blood;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy(gameObject, 1.5f);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("AI_Hit!!");
+                collision.GetComponentInParent<DeadCon>().decreaseHP();
+                Instantiate(blood[Random.Range(0, blood.Length)], new Vector2(this.transform.position.x, this.transform.position.y - 0.4f), this.transform.rotation);
+
+
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+
+
+
+}
---
+++ Subject12/Assets/Script/BulletBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public GameObject[] blood;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy(gameObject, 1.5f);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            if (collision.gameObject.tag == "AI")
+            {
+                Debug.Log("Hit!!");
+                Instantiate(blood[Random.Range(0, blood.Length)], new Vector2(this.transform.position.x,this.transform.position.y - 0.4f) , this.transform.rotation);
+
+                collision.GetComponentInParent<DeadCon>().decreaseHP();
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+
+
+
+}
